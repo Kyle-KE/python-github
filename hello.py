79a2a371cb6637c74d5552b613d5ac0c56d9dcb1
Kyle-KE <sawayakyle@gmail.com>
2023-01-27 17:14:24 +0300

    New Commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+print("Hello World!")
+
+# CammelCase
+# cpascalCase
+# snake_case
+#variables should be name in capital letters ?
+NAME="
+NUMBERS=0
+
+
+def another_hello_world(string:str):
+    print(string)
+
+
+if __name__ == "__main__":
+    another_hello_world("Hello")
